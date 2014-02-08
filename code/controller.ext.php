b486c95b05b1c0dd46878f8c36d798e814bc7a0d
jacobg830 <jacobg830@icloud.com>
2014-02-08 08:41:42 +0000

    Initial commit
    Set up project structure and completed basic XML info

---
+++ code/controller.ext.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * App Installer
+ * A Open Source Module for ZPanel
+ * Copyright (C) 2014 Jacob Gelling
+ * 
+ * This module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+class module_controller {
+
+    static function getModuleName() {
+        $module_name = ui_module::GetModuleName();
+        return $module_name;
+    }
+
+    static function getModuleIcon() {
+        global $controller;
+        $module_icon = "modules/" . $controller->GetControllerRequest('URL', 'module') . "/assets/icon.png";
+        return $module_icon;
+    }
+
+    static function getModuleDesc() {
+        $message = ui_language::translate(ui_module::GetModuleDescription());
+        return $message;
+    }
+    
+    static function getTopBar() {
+        return true;
+    }
+
+}
