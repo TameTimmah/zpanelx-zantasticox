ba59b11bfb2fa3c5fbc1984b7c157aed0c319367
jacobg830 <jacobg830@icloud.com>
2014-02-24 16:23:36 +0000

    Working category selection
    The code is getting messy though... will tidy up soon.

--- code/controller.ext.php
+++ code/controller.ext.php
@@ -45,33 +45,39 @@ class module_controller {
         $sql->execute();
         
         // Display 1st part of top bar
-        $html2 .= '<div id="app_topbar"><div class="pull-left"><select class="form-control"><option>All Applications</option>';
+        $html2 .= '<div id="app_topbar"><div class="pull-left"><select class="form-control" onchange="var str1 = \'?module=app_installer&query=\';var str2 = this.options[this.selectedIndex].value;location = str1.concat(str2);"><option>All Applications</option>';
         
         // For every category
         while ($rowdomains = $sql->fetch()) {
             // Add options to dropdown
-            $html2 .= '<option>'.$rowdomains['ai_name'].'</option>';
-            
-            $html .= '<section class="mainview">';
-            $html .= '<h3>'.$rowdomains['ai_name'].'</h3>';
-            $html .= '<p>'.$rowdomains['ai_desc'].'</p>';
-            
-            // Get apps in category
-            $types = unserialize(base64_decode($rowdomains['ai_types']));
-            $ids = "'". implode("', '", $types) ."'";
-            $query2 = "SELECT * FROM x_ai_apps WHERE ai_type IN ($ids)";
-            $sql2 = $zdbh->prepare($query2);
-            $sql2->execute();
-            
-            // For every app in category
-            while ($rowdomains2 = $sql2->fetch()) {
-                $html .= '<a href="?module=app_installer&app='.strtolower($rowdomains2['ai_name']).'">
-                    <img src="modules/app_installer/apps/'.strtolower($rowdomains2['ai_name']).'/smallicon.png" width="50" height="50" alt="'.$rowdomains2['ai_name'].'">
-                    <h5>'.$rowdomains2['ai_name'].'</h5>
-                    <h6>'.$rowdomains2['ai_type'].'</h6>
-                </a>';
+            if ($cat == $rowdomains['ai_name']) {
+                $html2 .= '<option selected>'.$rowdomains['ai_name'].'</option>';
+            }else {
+                $html2 .= '<option>'.$rowdomains['ai_name'].'</option>';
             }
-            $html .= '</section">';
+            
+            if ($cat == $rowdomains['ai_name'] || $cat == NULL || $cat == 'All Applications') {
+                $html .= '<section class="mainview">';
+                $html .= '<h3>'.$rowdomains['ai_name'].'</h3>';
+                $html .= '<p>'.$rowdomains['ai_desc'].'</p>';
+
+                // Get apps in category
+                $types = unserialize(base64_decode($rowdomains['ai_types']));
+                $ids = "'". implode("', '", $types) ."'";
+                $query2 = "SELECT * FROM x_ai_apps WHERE ai_type IN ($ids)";
+                $sql2 = $zdbh->prepare($query2);
+                $sql2->execute();
+
+                // For every app in category
+                while ($rowdomains2 = $sql2->fetch()) {
+                    $html .= '<a href="?module=app_installer&query='.strtolower($rowdomains2['ai_name']).'&act=view">
+                        <img src="modules/app_installer/apps/'.strtolower($rowdomains2['ai_name']).'/smallicon.png" width="50" height="50" alt="'.$rowdomains2['ai_name'].'">
+                        <h5>'.$rowdomains2['ai_name'].'</h5>
+                        <h6>'.$rowdomains2['ai_type'].'</h6>
+                    </a>';
+                }
+                $html .= '</section">';
+            }
         }
         
         // Display 2nd part of top bar
@@ -195,21 +201,21 @@ class module_controller {
     // Handles what is displayed
     static function getModuleDisplay() {
         // Add GET contents to variables
-        $app = $_GET['app'];
-        $act = $_GET['act'];
+        $query = htmlspecialchars($_GET['query']);
+        $act = htmlspecialchars($_GET['act']);
         
         // Decide what should be displayed
-        if($app==NULL & $action==NULL){
-            $html = module_controller::getMainView($app);
+        if($act==NULL){
+            $html = module_controller::getMainView($query);
         }
-        elseif($app!=NULL & $act==NULL){
-            $html = module_controller::getAppView($app);
+        elseif($query!=NULL & $act=='view'){
+            $html = module_controller::getAppView($query);
         }
-        elseif($app!=NULL & $act=='install'){
-            $html = module_controller::getAppInstall($app);
+        elseif($query!=NULL & $act=='install'){
+            $html = module_controller::getAppInstall($query);
         }
-        elseif($app!=NULL & $act=='search'){
-            $html = module_controller::getSearchResults($app);
+        elseif($query!=NULL & $act=='search'){
+            $html = module_controller::getSearchResults($query);
         }
         else{
             $html = module_controller::get404();
